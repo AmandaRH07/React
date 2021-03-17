502e8769ef742e9557e4761e4246360aae0ed290
AmandaRH07 <amandarafahass13@gmail.com>
2021-03-16 21:19:37 -0300

    Condicional e Repetição

--- Fundamentos_React/fundamentos-react/src/App.jsx
+++ Fundamentos_React/fundamentos-react/src/App.jsx
@@ -5,10 +5,28 @@ import Primeiro from './components/Primeiro'
 import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
 import Card from './components/layout/Card'
+import Repeticao from './components/Repeticao'
+import Condicional from './components/Condicional'
+import CondicionalComIF from './components/CondicionalComIF'
 
 export default (props) => (
     // jsx
     <div className="App">
+        <Card titulo="#06 - Condicionais versão 2">
+           <CondicionalComIF numero={10}>
+
+           </CondicionalComIF>
+        </Card>
+        <Card titulo="#05 - Condicionais versão 1">
+           <Condicional numero={11}>
+
+           </Condicional>
+        </Card>
+        <Card titulo="#04 - Repeticao">
+           <Repeticao>
+
+           </Repeticao>
+        </Card>
         <Card titulo="#03 - Componente com Filhos">
             <ComFilhos>
                 <ul>
