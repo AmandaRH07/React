8ddda1c37745444172145f1faaa5b793a5286924
Amanda Rafaela Hass <amanda.hass@hbsis.com.br>
2021-04-05 21:57:06 -0300

    Esqueleto destaque

--- Netflix/netflix_clone/src/App.js
+++ Netflix/netflix_clone/src/App.js
@@ -1,17 +1,26 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import './App.css'
+
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie'
 
 export default () => {
-  
+
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
-      //Pegando a lista total de filmes
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Pegando o Filme em destaque
+      let originals = list.filter(i=> i.slug === 'originals');
+      let randonChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randonChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
@@ -19,9 +28,14 @@ export default () => {
 
   return (
     <div className="page">
-       <section className="listas">
+
+      {featuredData && 
+        <FeaturedMovie item ={featuredData}/>
+      } 
+
+      <section className="listas">
         {movieList.map((item, key) => (
-          <MovieRow key={key} title={item.title} items={item.items}/>   
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
     </div>
